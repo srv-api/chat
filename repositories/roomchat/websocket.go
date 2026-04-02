74d22f2716a41ccf2b5170649ea0d252977732be
srv-kirim <inbox.kirim@gmail.com>
2026-04-02 16:30:52 +0700

    up to asc

--- repositories/roomchat/websocket.go
+++ repositories/roomchat/websocket.go
@@ -26,7 +26,7 @@ func (r *chatRepository) GetChatHistory(userID, receiverID string, limit, offset
 			"(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)",
 			userID, receiverID, receiverID, userID,
 		).
-		Order("created_at DESC").
+		Order("created_at ASC").
 		Limit(limit).
 		Offset(offset).
 		Find(&chats).Error
